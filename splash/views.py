2f1aa71a8099912cd3d120751308f1b93eeb76a5
jasonsbrooks <jason.brooks@yale.edu>
2013-11-08 22:45:58 -0500

    Recreated structure

--- splash/views.py
+++
@@ -1,8 +0,0 @@
-from flask import (Flask, render_template, Response, request, 
-    Blueprint, redirect, send_from_directory, send_file, jsonify, g)
-
-splash = Blueprint('splash', __name__, template_folder="")
-
-@splash.route('/')
-def home():
-	return render_template('templates/home.html');
