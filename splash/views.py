0fa38ee4f95ac6ae39ed39c2d08138c2ab022b00
jasonsbrooks <jason.brooks@yale.edu>
2013-11-08 23:13:21 -0500

    Hopefully app works now?

---
+++ splash/views.py
@@ -0,0 +1,10 @@
+from flask import (Flask, render_template, Response, request, 
+    Blueprint, redirect, send_from_directory, send_file, jsonify, g)
+
+
+
+splash = Blueprint('splash', __name__, template_folder="")
+
+@splash.route('/')
+def home():
+    return render_template('templates/home.html')
---
+++ webstart.py
@@ -0,0 +1,13 @@
+"""
+URL routing & initialization for webapp
+"""
+
+from os.path import join
+from main import app
+from flask import send_from_directory, Blueprint, send_file
+
+
+print "Starting webapp!"
+# splash
+from splash.views import splash
+app.register_blueprint(splash)
