7d243b8cbee4d82f5f24a0d5d8c3c1002703d212
jasonsbrooks <jason.brooks@yale.edu>
2013-11-09 00:34:51 -0500

    Added amazon abilities

--- webstart.py
+++ webstart.py
@@ -12,5 +12,11 @@ print "Starting webapp!"
 from splash.views import splash
 app.register_blueprint(splash)
 
+# modules manage their own static files. This serves them all up.
+@app.route("/<blueprint_name>/static/<path:fn>")
+def _return_static(blueprint_name, fn='index.html'):
+    path = join(*app.blueprints[blueprint_name].import_name.split('.')[:-1]) 
+    return send_file('%s/static/%s' % (path, fn)) 
+
 if __name__ == '__main__':
     app.run()
